345f29882f62b3e7c58ee818688c75e8292dd455
jchan3 <joechan2014@gmail.com>
2016-10-01 22:48:17 +0000

    task 01

---
+++ task_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for task_01."""
+
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon')
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division', 'Addition', 'Subtraction']
